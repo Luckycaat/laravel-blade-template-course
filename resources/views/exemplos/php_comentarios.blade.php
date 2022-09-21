0294effb312e256263a8102588d81a41856954b1
Pedro <pedro.de@construsitebrasil.com.br>
2022-09-21 09:01:51 -0300

    Exibição de Informações

---
+++ resources/views/exemplos/php_comentarios.blade.php
@@ -0,0 +1,28 @@
+<?php
+
+$a = 10;
+$b = 20;
+
+$c = $a + $b;
+
+echo $c;
+
+?>
+
+<br>
+
+@php
+
+$a = 100;
+$b = 200;
+
+$c = $a + $b;
+
+echo $c;
+
+@endphp
+
+
+{{-- este é um comentário interno --}}
+
+<!-- Este é um comentário HTMl -->
--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,30 @@ Route::get('/', function () {
 
 Route::get('/passagem/dados', function () {
     return view('exemplos.passagem_dados', [
-        'nome' => 'Pedro',
+        'nome' => 'Pedro <script>alert("Executei")</script>',
         'descricao' => 'Colaborador Construsite',
     ]);
 });
 
+Route::get('/exibicao/json', function () {
+    return view('exemplos.exibicao_json')->with([
+        'posts' => [
+            [
+                'titulo' => 'Novidades do Laravel',
+                'conteudo' => 'Nesta versão o Laravel...'
+            ],
+            [
+                'titulo' => 'Novidades do Blade',
+                'conteudo' => 'Nesta versão o Blade...'
+            ],
+        ],
+    ]);
+});
 
+Route::get('/frameworks/js', function () {
+    return view('exemplos.frameworks_js');
+});
+
+Route::get('/php/comentarios', function () {
+    return view('exemplos.php_comentarios');
+});
